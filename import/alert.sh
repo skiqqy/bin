541474fb4ec7df35c9699c4f29ff8370401b2a2e
Stephen Cochrane <skippycochrane@gmail.com>
2020-11-20 23:09:04 +0200

    Added asserts

--- import/alert.sh
+++ import/alert.sh
@@ -4,6 +4,7 @@
 # USAGE: error "message" <exit_code>
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
+GREEN='\e[92m'
 NC='\033[0m'
 
 # Display an error message and exit.
@@ -15,4 +16,12 @@ error () {
 
 warning () {
 	printf "[${YELLOW}WARNING${NC}] $1\n"
+}
+
+success () {
+	printf "[${GREEN}SUCCESS${NC}] $1\n"
+}
+
+tick () {
+	printf "$1 ${GREEN}✔${NC}\n"
 }
